a8d19c7ae12a6189fa803b202ca088f4f4641715
Glen Andrew <gcbulaong@up.edu.ph>
2024-10-16 10:42:54 +0800

    feat: create dru model

---
+++ app/denguedash_backend/api/models/dru.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class DRU(models.Model):
+
+    region = models.CharField(
+        max_length=50,
+        blank=False,
+        null=False,
+        default="Western Visayas",
+    )
+    province = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        default="Iloilo",
+    )
+    municipality = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        default="Iloilo City",
+    )
+    dru = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+    )
+    address = models.TextField(
+        blank=False,
+        null=False,
+    )
+    dru_types = [
+        ("rhu", "RHU"),
+        ("ho", "CHO/MHO/PHO"),
+        ("government_hospital", "Government Hospital"),
+        ("private_hospital", "Private Hospital"),
+        ("clinic", "Clinic"),
+    ]
+    dru_type = models.CharField(
+        max_length=20,
+        choices=dru_types,
+        blank=False,
+        null=False,
+    )
+
+    def __str__(self):
+        return self.dru
