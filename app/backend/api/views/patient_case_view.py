cb87bc43cabb658bbecfe41ca49bb9bad1749df8
Glen Andrew <gcbulaong@up.edu.ph>
2024-10-17 15:26:32 +0800

    feat: create view for patient case

---
+++ app/backend/api/views/patient_case_view.py
@@ -0,0 +1,26 @@
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from ..serializers.patient_case_serializer import CaseSerializer
+
+
+class PatientCaseView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request):
+        serializer = CaseSerializer(
+            data=request.data,
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"success": True},
+                status=status.HTTP_201_CREATED,
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
