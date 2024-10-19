2f243a497ee14c441930c3b5afd8c45f44171c71
Glen Andrew <gcbulaong@up.edu.ph>
2024-10-19 23:03:02 +0800

    chore: configurate nuxt-ui

--- app/frontend/denguedash/nuxt.config.ts
+++ app/frontend/denguedash/nuxt.config.ts
@@ -1,10 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+  colorMode: {
+    preference: "light",
+    fallback: "light",
+    classSuffix: "",
+  },
   devtools: {
     enabled: true,
   },
-  modules: ["@nuxt/eslint", "@nuxtjs/leaflet"],
+  modules: ["@nuxt/eslint", "@nuxtjs/leaflet", "@nuxt/ui"],
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
