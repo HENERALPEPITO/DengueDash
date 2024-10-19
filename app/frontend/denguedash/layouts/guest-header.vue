7c2ce0da14e9710f6a225228cde69feda69c3c2d
Glen Andrew <gcbulaong@up.edu.ph>
2024-10-19 18:53:12 +0800

    chore: convert component into layout

---
+++ app/frontend/denguedash/layouts/guest-header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header class="bg-blue-800 text-white">
+      <nav
+        class="container w-5/6 mx-auto flex items-center justify-between py-6"
+      >
+        <NuxtLink to="/" class="text-2xl font-bold">DengueDash</NuxtLink>
+        <ul class="flex space-x-6">
+          <li>
+            <NuxtLink to="/" class="hover:underline">Dashboard</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/" class="hover:underline">Login</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
