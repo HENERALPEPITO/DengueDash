99c4363536a132b36cfec87a5c9bac82bdf29754
Glen Andrew <gcbulaong@up.edu.ph>
2024-10-19 19:12:03 +0800

    chore: install shadcn

--- app/frontend/denguedash/nuxt.config.ts
+++ app/frontend/denguedash/nuxt.config.ts
@@ -4,12 +4,23 @@ export default defineNuxtConfig({
   devtools: {
     enabled: true,
   },
-  modules: ["@nuxt/eslint", "@nuxtjs/leaflet"],
+  modules: ["@nuxt/eslint", "@nuxtjs/leaflet", "shadcn-nuxt"],
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
   },
 });
