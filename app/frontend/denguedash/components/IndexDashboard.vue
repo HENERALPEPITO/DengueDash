06ca83db4919c1f1a7f5e6d537101e1c41ac678f
Glen Andrew <gcbulaong@up.edu.ph>
2024-10-19 18:54:14 +0800

    style: card border

--- app/frontend/denguedash/components/IndexDashboard.vue
+++ app/frontend/denguedash/components/IndexDashboard.vue
@@ -1,22 +1,11 @@
 <template>
-  <!-- <div class="w-1/2 p-3">
-    <div class="border-grey border-2 rounded-lg">
-      <div class="p-4">
-        <h1 class="text-xl font-bold">Number of Dengue Cases in Iloilo City</h1>
-        <p class="text-sm text-gray-600">As of October 19, 2024</p>
-      </div>
-      <div class="flex justify-center items-center"></div>
-      <ChoroplethMap />
-    </div>
-  </div> -->
-
   <div class="p-6 min-h-screen">
     <div class="w-5/6 mx-auto">
       <div class="flex flex-col lg:flex-row gap-6">
         <!-- Choropleth Map -->
-        <div class="border-grey border-2 rounded-lg flex-1">
+        <div class="border border-grey rounded-lg flex-1">
           <div class="p-4">
-            <h1 class="text-xl font-bold">
+            <h1 class="text-2xl font-bold">
               Number of Dengue Cases in Iloilo City
             </h1>
             <p class="text-sm text-gray-600">As of October 19, 2024</p>
@@ -24,7 +13,7 @@
           <ChoroplethMap />
         </div>
         <!-- Right Side -->
-        <div class="bg-white shadow-lg rounded-lg lg:w-96 flex-1">
+        <div class="border border-grey rounded-lg flex-1">
           <div class="p-6">
             <h2 class="text-2xl font-bold">
               322,343 <span class="text-red-500">+63,728</span>
