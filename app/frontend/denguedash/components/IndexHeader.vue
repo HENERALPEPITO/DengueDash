e2d90c5d51656befbb6c59af4bb62b7cd01312e6
Glen Andrew <gcbulaong@up.edu.ph>
2024-10-19 15:10:56 +0800

    feat: create header component

--- app/frontend/denguedash/components/IndexHeader.vue
+++ app/frontend/denguedash/components/IndexHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="bg-blue-800 text-white">
-    <nav class="container mx-auto flex items-center justify-between py-4">
+    <nav class="container mx-auto flex items-center justify-between py-6">
       <NuxtLink to="/" class="text-2xl font-bold">DengueDash</NuxtLink>
       <ul class="flex space-x-6">
         <li>
