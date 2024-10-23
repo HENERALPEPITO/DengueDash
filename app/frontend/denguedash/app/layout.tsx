0305a840e4cfd820fe00301b4a3aec7d8a8d51ca
Glen Andrew <gcbulaong@up.edu.ph>
2024-10-24 00:01:53 +0800

    chore: import leaflet css

--- app/frontend/denguedash/app/layout.tsx
+++ app/frontend/denguedash/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import "leaflet/dist/leaflet.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
