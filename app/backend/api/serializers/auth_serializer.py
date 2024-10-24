b4faf7163e0c1c456c20a16693fa6b4c3e587bd9
Glen Andrew <gcbulaong@up.edu.ph>
2024-10-25 01:02:32 +0800

    fix: use custom exception to make success boolean

--- app/backend/api/serializers/auth_serializer.py
+++ app/backend/api/serializers/auth_serializer.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.exceptions import AuthenticationFailed
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from ..custom_exceptions.custom_validation_exception import CustomValidationException
 from ..models.user import UserClassification
 from ..models.dru import DRU
 
@@ -75,18 +76,12 @@ class LoginSerializer(TokenObtainPairSerializer):
         try:
             data = super().validate(attrs)
         except AuthenticationFailed:
-            raise AuthenticationFailed(
-                {
-                    "success": False,
-                    "message": "No active account found with the given credentials",
-                }
+            raise CustomValidationException(
+                "No active account found with the given credentials"
             )
         except Exception:
-            raise AuthenticationFailed(
-                {
-                    "success": False,
-                    "message": "An unexpected error occurred during login.",
-                }
+            raise CustomValidationException(
+                "An unexpected error occurred during login."
             )
         user = self.user
         user.last_login = timezone.now()
