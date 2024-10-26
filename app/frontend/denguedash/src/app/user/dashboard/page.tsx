ee84761512b2b3d89d93a0834a5e20872b310505
Glen Andrew <gcbulaong@up.edu.ph>
2024-10-27 00:10:49 +0800

    feat: integrate dashboard to page

--- app/frontend/denguedash/src/app/user/dashboard/page.tsx
+++ app/frontend/denguedash/src/app/user/dashboard/page.tsx
@@ -1,3 +1,9 @@
+import StatDashboard from "@/app/components/stat_dashboard/StatDashboard";
+
 export default function Dashboard() {
-  return <div>Hello Dashboard</div>;
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+      <StatDashboard />
+    </div>
+  );
 }
