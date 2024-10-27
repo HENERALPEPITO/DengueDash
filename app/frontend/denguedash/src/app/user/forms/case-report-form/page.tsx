cef1648ccb5e6e051aee564894ec5811b6e9fe43
Glen Andrew <gcbulaong@up.edu.ph>
2024-10-27 22:52:47 +0800

    style: center the content horizontally

--- app/frontend/denguedash/src/app/user/forms/case-report-form/page.tsx
+++ app/frontend/denguedash/src/app/user/forms/case-report-form/page.tsx
@@ -2,14 +2,14 @@ import MultiStepForm from "@components/case-report-form/MultiStepForm";
 
 export default function CaseReportForm() {
   return (
-    <div className="flex flex-1 flex-col gap-4 px-5 pt-2">
-      <div className="border border-grey rounded-lg p-5">
-        {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+    <div className="h-screen flex justify-center gap-4 px-5 pt-2">
+      {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
       </div>
       <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" /> */}
+      <div className="w-5/6">
         <MultiStepForm />
       </div>
     </div>
