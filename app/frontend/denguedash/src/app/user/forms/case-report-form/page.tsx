220e9119c0113ee9e063825b0f6a5b9e4b647234
Glen Andrew <gcbulaong@up.edu.ph>
2024-10-28 10:30:52 +0800

    style: remove h-screen

--- app/frontend/denguedash/src/app/user/forms/case-report-form/page.tsx
+++ app/frontend/denguedash/src/app/user/forms/case-report-form/page.tsx
@@ -2,7 +2,7 @@ import MultiStepForm from "@components/case-report-form/MultiStepForm";
 
 export default function CaseReportForm() {
   return (
-    <div className="h-screen flex justify-center gap-4 px-5 pt-2">
+    <div className="flex justify-center gap-4 px-5 pt-2 pb-5">
       {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
