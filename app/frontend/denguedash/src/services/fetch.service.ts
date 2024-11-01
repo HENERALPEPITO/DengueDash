3b38f08e9c09425a78e515e94a2e70460c91cca5
Glen Andrew <gcbulaong@up.edu.ph>
2024-11-02 03:45:16 +0800

    feat: add get dengue data wrapper

--- app/frontend/denguedash/src/services/fetch.service.ts
+++ app/frontend/denguedash/src/services/fetch.service.ts
@@ -12,6 +12,19 @@ const getDengueReports = async (page: number, itemsPerPage: number = 10) => {
   }
 };
 
-const getService = { getDengueReports };
+const getDengueCountPerBarangay = async (year: number | null = null) => {
+  try {
+    const yearQuery = year ? `?year=${year}` : "";
+    const response = await axiosInstance.get(
+      `cases-per-barangay`.concat(yearQuery)
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching dengue count per barangay:", error);
+    throw error;
+  }
+};
 
-export default getService;
+const fetchService = { getDengueReports, getDengueCountPerBarangay };
+
+export default fetchService;
