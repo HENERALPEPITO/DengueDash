1b88c79cebd85e5c0f904a0ab97737a82a234b45
Glen Andrew <gcbulaong@up.edu.ph>
2024-11-02 00:40:19 +0800

    refactor: remove template divs

--- app/frontend/denguedash/src/app/user/forms/case-report-form/page.tsx
+++ app/frontend/denguedash/src/app/user/forms/case-report-form/page.tsx
@@ -3,12 +3,6 @@ import MultiStepForm from "@components/case-report-form/MultiStepForm";
 export default function CaseReportForm() {
   return (
     <div className="flex justify-center gap-4 px-5 pt-2 pb-5">
-      {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-      </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" /> */}
       <div className="w-5/6">
         <MultiStepForm />
       </div>
