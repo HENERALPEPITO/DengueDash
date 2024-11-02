dc0856650829711364a53f55c6202ed4ba9742a9
Glen Andrew <gcbulaong@up.edu.ph>
2024-11-03 05:37:31 +0800

    feat: create yearly dengue count serializer

--- app/backend/api/serializers/dengue_count_serializer.py
+++ app/backend/api/serializers/dengue_count_serializer.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 
 
-class DengueCountSerializer(serializers.Serializer):
+class MapDengueCountSerializer(serializers.Serializer):
     barangay = serializers.CharField()
     case_count = serializers.IntegerField()
+
+
+class YearlyDengueCountSerializer(serializers.Serializer):
+    year = serializers.IntegerField()
+    case_count = serializers.IntegerField()
+    death_count = serializers.IntegerField()
