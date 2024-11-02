3b1d5f0da0b66f6d88c4c5b37715255d5e12c2d9
Glen Andrew <gcbulaong@up.edu.ph>
2024-11-03 05:38:13 +0800

    feat: view for yearly dengue cases

--- app/backend/api/views/dengue_count_view.py
+++ app/backend/api/views/dengue_count_view.py
@@ -1,9 +1,12 @@
-from django.db.models import Count
+from django.db.models import Count, Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
 from api.models.case import Case
-from api.serializers.dengue_count_serializer import DengueCountSerializer
+from api.serializers.dengue_count_serializer import (
+    MapDengueCountSerializer,
+    YearlyDengueCountSerializer,
+)
 
 
 class BarangayCountView(APIView):
@@ -27,6 +30,45 @@ class BarangayCountView(APIView):
             for item in cases_per_barangay
         ]
 
-        # Serialize the data
-        serializer = DengueCountSerializer(data, many=True)
+        serializer = MapDengueCountSerializer(data, many=True)
+        return Response(serializer.data)
+
+
+class YearlyDengueCountStatView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, *args, **kwargs):
+        start_year = 2016
+
+        # Aggregate cases per year
+        cases_per_year = (
+            Case.objects.filter(date_con__year__gte=start_year)
+            .values("date_con__year")
+            .annotate(case_count=Count("case_id"))
+            .order_by("date_con__year")
+        )
+
+        # Aggregate deaths per year
+        death_per_year = (
+            Case.objects.filter(date_con__year__gte=start_year)
+            .values("date_con__year")
+            .annotate(death_count=Count("case_id", filter=Q(outcome="D")))
+            .order_by("date_con__year")
+        )
+
+        # Convert death_per_year to a dictionary for easy lookup
+        death_dict = {
+            item["date_con__year"]: item["death_count"] for item in death_per_year
+        }
+
+        data = [
+            {
+                "year": item["date_con__year"],
+                "case_count": item["case_count"],
+                "death_count": death_dict.get(item["date_con__year"], 0),
+            }
+            for item in cases_per_year
+        ]
+
+        serializer = YearlyDengueCountSerializer(data, many=True)
         return Response(serializer.data)
