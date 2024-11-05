5c0fc0ab29fcf741cc7300fc2a0745acd46b85da
Glen Andrew <gcbulaong@up.edu.ph>
2024-11-05 20:59:00 +0800

    chore: remove logging message

--- app/backend/api/middleware.py
+++ app/backend/api/middleware.py
@@ -9,9 +9,9 @@ class JWTAuthMiddleware(MiddlewareMixin):
         access_token = request.COOKIES.get("access_token")
         if access_token:
             # Log the token to confirm it’s being extracted
-            logger.info(f"Access token found: {access_token}")
+            # logger.info(f"Access token found: {access_token}")
 
             # Add the token to the Authorization header
             request.META["HTTP_AUTHORIZATION"] = f"Bearer {access_token}"
-        else:
-            logger.warning("No access token found in cookies.")
+        # else:
+        #     logger.warning("No access token found in cookies.")
