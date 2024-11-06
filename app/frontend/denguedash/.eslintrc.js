ff4e847689de268c3bd693a9710c441db0adbe61
Glen Andrew <gcbulaong@up.edu.ph>
2024-11-06 20:31:29 +0800

    chore: turn off "any" highlight error

--- app/frontend/denguedash/.eslintrc.js
+++ app/frontend/denguedash/.eslintrc.js
@@ -17,6 +17,6 @@ module.exports = {
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
-    // "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
