246fcdeccb5f580893b0464721158b57ee7ce428
Glen Andrew <gcbulaong@up.edu.ph>
2024-11-06 20:19:55 +0800

    style: mobile view style

--- app/frontend/denguedash/src/components/stat_dashboard/StatCard.tsx
+++ app/frontend/denguedash/src/components/stat_dashboard/StatCard.tsx
@@ -10,12 +10,12 @@ type StatCardProps = {
 export default function StatCard({ title, value, icon }: StatCardProps) {
   return (
     <Card className="w-full">
-      <div className="h-[15vh] px-6 py-7">
+      <div className="h-[12vh] px-3 lg:px-6 py-3">
         <div className="flex justify-between items-center">
-          <h1 className="text-lg font-semibold">{title}</h1>
+          <h1 className="text-sm lg:text-lg font-semibold">{title}</h1>
           <Icon icon={icon} className="text-2xl" />
         </div>
-        <p className="mt-3 text-3xl font-medium">{value}</p>
+        <p className="text-2xl lg:text-3xl font-medium mt-1 lg:mt-3">{value}</p>
       </div>
     </Card>
   );
