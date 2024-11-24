d9b42d187f9d21e5d787b9a4e6a0cfba8a270bb1
Glen Andrew <gcbulaong@up.edu.ph>
2024-11-25 00:07:04 +0800

    refactor: adapt to druType table

--- app/backend/seeders/management/commands/dru_seeder.py
+++ app/backend/seeders/management/commands/dru_seeder.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from dru.models import DRU
+from dru.models import DRU, DRUType
 
 
 class Command(BaseCommand):
@@ -8,24 +8,34 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         drus = [
             {
-                "name": "Iloilo Epidemiology and Surveillance Unit",
-                "address": "Plaza Libertad",
-                "type": "CHO/MHO/PHO",
+                "name": "Western Visayas Regional Epedemiology and Surveillance Unit",
+                "address": "MHAM Building, Iloilo City",
+                "type": DRUType.objects.get(id=1),
+            },
+            {
+                "name": "Iloilo Provincial Epedemiology and Surveillance Unit",
+                "address": "Provincial Capitol, Iloilo City",
+                "type": DRUType.objects.get(id=2),
+            },
+            {
+                "name": "Iloilo City Epedemiology and Surveillance Unit",
+                "address": "City Hall, Iloilo City",
+                "type": DRUType.objects.get(id=2),
             },
             {
                 "name": "Molo District Health Center",
                 "address": "Locsin Street, Molo",
-                "type": "CHO/MHO/PHO",
+                "type": DRUType.objects.get(id=4),
             },
             {
                 "name": "Jaro 1 Health Center",
                 "address": "Washington Street, Jaro",
-                "type": "CHO/MHO/PHO",
+                "type": DRUType.objects.get(id=4),
             },
             {
                 "name": "Saint Paul's Hospital",
                 "address": "General Luna Street",
-                "type": "priv_hsptl",
+                "type": DRUType.objects.get(id=6),
             },
         ]
 
