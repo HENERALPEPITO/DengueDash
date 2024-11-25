0257acb2ad7bcb51c6b1c42cb3e20647104029df
Glen Andrew <gcbulaong@up.edu.ph>
2024-11-25 21:59:33 +0800

    chore: adapt dru_seeder with recent changes

--- app/backend/seeders/management/commands/dru_seeder.py
+++ app/backend/seeders/management/commands/dru_seeder.py
@@ -11,31 +11,37 @@ class Command(BaseCommand):
                 "name": "Western Visayas Regional Epedemiology and Surveillance Unit",
                 "address": "MHAM Building, Iloilo City",
                 "type": DRUType.objects.get(id=1),
+                "unit": None,
             },
             {
                 "name": "Iloilo Provincial Epedemiology and Surveillance Unit",
                 "address": "Provincial Capitol, Iloilo City",
                 "type": DRUType.objects.get(id=2),
+                "unit": "Iloilo Province",
             },
             {
                 "name": "Iloilo City Epedemiology and Surveillance Unit",
                 "address": "City Hall, Iloilo City",
                 "type": DRUType.objects.get(id=2),
+                "unit": "Iloilo City",
             },
             {
                 "name": "Molo District Health Center",
                 "address": "Locsin Street, Molo",
                 "type": DRUType.objects.get(id=4),
+                "unit": "Iloilo City",
             },
             {
                 "name": "Jaro 1 Health Center",
                 "address": "Washington Street, Jaro",
                 "type": DRUType.objects.get(id=4),
+                "unit": "Iloilo City",
             },
             {
                 "name": "Saint Paul's Hospital",
                 "address": "General Luna Street",
                 "type": DRUType.objects.get(id=6),
+                "unit": "Iloilo City",
             },
         ]
 
@@ -44,6 +50,7 @@ class Command(BaseCommand):
                 dru=dru["name"],
                 address=dru["address"],
                 dru_type=dru["type"],
+                surveillance_unit=dru["unit"],
             )
 
         self.stdout.write(
