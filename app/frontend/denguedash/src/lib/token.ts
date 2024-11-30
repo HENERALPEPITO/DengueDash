0dbdadfbea54892ce11ea61cef2b715be7057512
Glen Andrew <gcbulaong@up.edu.ph>
2024-11-30 17:20:11 +0800

    feat (frontend): verify token signature

--- app/frontend/denguedash/src/lib/token.ts
+++ app/frontend/denguedash/src/lib/token.ts
@@ -1,4 +1,16 @@
-import { decodeJwt } from "jose";
+import { decodeJwt, jwtVerify } from "jose";
+
+export const verifyTokenSignature = async (token: string): Promise<boolean> => {
+  const SECRET_KEY = process.env.SECRET_KEY;
+  try {
+    await jwtVerify(token, new TextEncoder().encode(SECRET_KEY));
+    console.log("Token signature verified");
+    return true;
+  } catch (error) {
+    console.log("Cannot verify token signature:", error);
+    return false;
+  }
+};
 
 export const validateToken = async (token: string): Promise<boolean> => {
   try {
--- app/frontend/denguedash/src/middleware.ts
+++ app/frontend/denguedash/src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-import { validateToken } from "./lib/token";
+import { validateToken, verifyTokenSignature } from "./lib/token";
 import { cookies } from "next/headers";
 
 type refreshTokenResponse = {
@@ -11,8 +11,26 @@ export async function middleware(request: NextRequest, event: NextFetchEvent) {
   const accessToken = cookieStore.get("access_token");
   const refreshToken = cookieStore.get("refresh_token");
 
+  const deleteCookies = () => {
+    cookieStore.delete("access_token");
+    cookieStore.delete("refresh_token");
+  };
+
   // Check if tokens exist
   if (!accessToken || !refreshToken) {
+    deleteCookies();
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // Verify the signatures for both tokens
+  const isAccessTokenSignatureValid = await verifyTokenSignature(
+    accessToken.value
+  );
+  const isRefreshTokenSignatureValid = await verifyTokenSignature(
+    refreshToken.value
+  );
+  if (!isAccessTokenSignatureValid || !isRefreshTokenSignatureValid) {
+    deleteCookies();
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
@@ -20,8 +38,7 @@ export async function middleware(request: NextRequest, event: NextFetchEvent) {
 
   // Verify if refresh token is expired
   if (isRefreshTokenExpired) {
-    cookieStore.delete("access_token");
-    cookieStore.delete("refresh_token");
+    deleteCookies();
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
@@ -59,6 +76,7 @@ export async function middleware(request: NextRequest, event: NextFetchEvent) {
       }
     } catch (error) {
       console.error("Error refreshing access token:", error);
+      deleteCookies();
       return NextResponse.redirect(new URL("/login", request.url));
     }
   }
