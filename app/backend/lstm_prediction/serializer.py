4b9f1cc2e68cb75db9fec566dbf9adcf33b9b5df
Glen Andrew <gcbulaong@up.edu.ph>
2025-02-24 09:57:40 +0800

    feat: update serializer

--- app/backend/lstm_prediction/serializer.py
+++ app/backend/lstm_prediction/serializer.py
@@ -1,6 +1,17 @@
 from rest_framework import serializers
 
-class LstmPredictionSerializer(serializers.Serializer):
-    predicted_cases_next_8_weeks = serializers.ListField(
-        child = serializers.IntegerField()
-    )
+class WeatherDataSerializer(serializers.Serializer):
+    rainfall = serializers.FloatField()
+    max_temperature = serializers.FloatField()
+    humidity = serializers.FloatField()
+
+class HistoricalDataSerializer(serializers.Serializer):
+    rainfall = serializers.FloatField()
+    max_temperature = serializers.FloatField()
+    humidity = serializers.FloatField()
+    cases = serializers.FloatField()
+
+class PredictionRequestSerializer(serializers.Serializer):
+    historical_data = HistoricalDataSerializer(many=True)
+    future_weather = WeatherDataSerializer(many=True)
+    last_date = serializers.DateField()
