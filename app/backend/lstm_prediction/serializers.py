a3ae91c8dc5b191eefc446cc59f956a0b8b8f406
Glen Andrew <gcbulaong@up.edu.ph>
2025-02-28 06:49:46 +0800

    feat: remove unnecessary serializers

--- app/backend/lstm_prediction/serializers.py
+++ app/backend/lstm_prediction/serializers.py
@@ -7,14 +7,5 @@ class WeatherDataSerializer(serializers.Serializer):
     humidity = serializers.FloatField()
 
 
-class HistoricalDataSerializer(serializers.Serializer):
-    rainfall = serializers.FloatField()
-    max_temperature = serializers.FloatField()
-    humidity = serializers.FloatField()
-    cases = serializers.FloatField()
-
-
 class PredictionRequestSerializer(serializers.Serializer):
-    # historical_data = HistoricalDataSerializer(many=True)
     future_weather = WeatherDataSerializer(many=True)
-    last_date = serializers.DateField()
