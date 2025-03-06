45d234cbec2c7cb0b23b18b3620d7dac69c2bf7a
Glen Andrew <gcbulaong@up.edu.ph>
2025-03-06 13:06:47 +0800

    refactor: use wrapper functions

--- app/frontend/denguedash/src/services/fetch.service.ts
+++ app/frontend/denguedash/src/services/fetch.service.ts
@@ -5,14 +5,6 @@ const DATA = null;
 
 // Requests that does not need authentication
 const getQuickStat = async (year: number | null = null) => {
-  // try {
-  //   const yearQuery = year ? `?year=${year}` : "";
-  //   const response = await axiosOpen.get(`quick-stat`.concat(yearQuery));
-  //   return response.data;
-  // } catch (error) {
-  //   console.error("Error fetching current case count:", error);
-  //   throw error;
-  // }
   return axiosClient(
     "quick-stat",
     OPERATION,
@@ -23,16 +15,6 @@ const getQuickStat = async (year: number | null = null) => {
 };
 
 const getDengueCountPerBarangay = async (year: number | null = null) => {
-  // try {
-  //   const yearQuery = year ? `?year=${year}` : "";
-  //   const response = await axiosOpen.get(
-  //     `cases-per-barangay`.concat(yearQuery)
-  //   );
-  //   return response.data;
-  // } catch (error) {
-  //   console.error("Error fetching dengue count per barangay:", error);
-  //   throw error;
-  // }
   return axiosClient(
     "cases-per-barangay",
     OPERATION,
@@ -43,14 +25,6 @@ const getDengueCountPerBarangay = async (year: number | null = null) => {
 };
 
 const getCasesDeaths = async (year: number | null = null) => {
-  // try {
-  //   const yearQuery = year ? `?year=${year}` : "";
-  //   const response = await axiosOpen.get(`cases-deaths`.concat(yearQuery));
-  //   return response.data;
-  // } catch (error) {
-  //   console.error("Error fetching dengue cases and deaths count:", error);
-  //   throw error;
-  // }
   return axiosClient(
     "cases-deaths",
     OPERATION,
@@ -62,19 +36,6 @@ const getCasesDeaths = async (year: number | null = null) => {
 
 // Requests that must need authentication
 const getDengueReports = async (page: number, itemsPerPage: number = 10) => {
-  // try {
-  // const response = await axiosInstance.get(
-  //   `dengue-case-reports?page=${page}&page_size=${itemsPerPage}`
-  // );
-  //   return response.data;
-  // } catch (error) {
-  //   console.error("Error fetching dengue reports:", error);
-  //   throw error;
-  // }
-  // return axiosInterceptor("dengue-case-reports", {
-  //   page,
-  //   page_size: itemsPerPage,
-  // });
   return axiosClient("dengue-case-reports", OPERATION, DATA, {
     page,
     itemsPerPage,
@@ -82,13 +43,6 @@ const getDengueReports = async (page: number, itemsPerPage: number = 10) => {
 };
 
 const getCaseViewDetails = async (caseId: number) => {
-  // try {
-  //   const response = await axiosInstance.get(`dengue-case-reports/${caseId}`);
-  //   return response.data;
-  // } catch (error) {
-  //   console.error("Error fetching case details", error);
-  //   throw error;
-  // }
   return axiosClient("dengue-case-reports", OPERATION, DATA, { caseId });
 };
 
