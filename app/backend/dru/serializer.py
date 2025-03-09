a9f2b74415c2181dab229be367bc4f433e0037ff
Glen Andrew <gcbulaong@up.edu.ph>
2025-03-09 18:39:58 +0800

    feat: register DRU

--- app/backend/dru/serializer.py
+++ app/backend/dru/serializer.py
@@ -15,7 +15,40 @@ class DRUTypeSerializer(serializers.ModelSerializer):
 
 
 class RegisterDRUSerializer(serializers.ModelSerializer):
+    dru_type = serializers.PrimaryKeyRelatedField(
+        queryset=DRUType.objects.all(),
+        required=True,
+    )
     class Meta:
         model = DRU
-        fields = "__all__"
-        read_only_fields = ["created_at", "updated_at"]
+        fields = [
+            "region",
+            "surveillance_unit",
+            "dru_name",
+            "address",
+            "email",
+            "contact_number",
+            "dru_type",
+        ]
+
+    def validate(self, data):
+        # Todo: Validate contact number
+        
+        dru_name = data.get("dru_name")
+        surveillance_unit = data.get("surveillance_unit")
+        if DRU.objects.filter(
+            surveillance_unit=surveillance_unit,
+            dru_name=dru_name,
+        ).exists():
+            raise serializers.ValidationError(
+                {
+                    "success" : False,
+                    "message" : "DRU already exists"
+                }
+            )
+        
+        return data
+        
+    def create(self, validated_data):
+        dru = DRU.objects.create(**validated_data)
+        return dru
