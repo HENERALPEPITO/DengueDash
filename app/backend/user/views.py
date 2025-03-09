37be76500937d7283f0fb30a96971f26d8b8e64e
Glen Andrew <gcbulaong@up.edu.ph>
2025-03-09 19:39:29 +0800

    feat: convert the process to sign up process

--- app/backend/user/views.py
+++ app/backend/user/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from django.http import JsonResponse
 from user.serializers import (
     UserSerializer,
+    RegisterSerializer,
 )
 from dru.models import DRU
 
@@ -15,39 +16,52 @@ class UserDetailView(APIView):
         return Response(serializer.data)
 
 class RegisterUserView(APIView):
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
     def post(self, request):
-        current_user = request.user
-        if not current_user.is_admin:
-            return JsonResponse(
-                {
-                    "success": False,
-                    "message": "You are not authorized to perform this action",
-                },
-            )
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({
+                "success": True,
+                "message": "User registered successfully",
+            })
+        
+        return JsonResponse({
+            "success": False,
+            "message": serializer.errors
+        })
+    
+        # current_user = request.user
+        # if not current_user.is_admin:
+        #     return JsonResponse(
+        #         {
+        #             "success": False,
+        #             "message": "You are not authorized to perform this action",
+        #         },
+        #     )
 
-        user = request.user
+        # user = request.user
 
-        if "dru" not in request.data:
-            return JsonResponse(
-                {
-                    "success": False,
-                    "message": "DRU is required",
-                },
-            )
+        # if "dru" not in request.data:
+        #     return JsonResponse(
+        #         {
+        #             "success": False,
+        #             "message": "DRU is required",
+        #         },
+        #     )
 
-        dru_id = request.data.get("dru")
-        try:
-            dru_to_register = DRU.objects.filter(id=dru_id).first()
-            current_user_dru = user.dru
-        except DRU.DoesNotExist:
-            return JsonResponse(
-                {
-                    "success": False,
-                    "message": "DRU not found",
-                },
-            )
+        # dru_id = request.data.get("dru")
+        # try:
+        #     dru_to_register = DRU.objects.filter(id=dru_id).first()
+        #     current_user_dru = user.dru
+        # except DRU.DoesNotExist:
+        #     return JsonResponse(
+        #         {
+        #             "success": False,
+        #             "message": "DRU not found",
+        #         },
+        #     )
 
         # return JsonResponse(
         #     {
