d26c8b1dba27a9b8d7374e43b717232c702f6ead
Glen Andrew <gcbulaong@up.edu.ph>
2025-03-10 11:47:59 +0800

    feat: remove unnecessary code

--- app/backend/user/views.py
+++ app/backend/user/views.py
@@ -8,12 +8,14 @@ from user.serializers import (
 )
 from dru.models import DRU
 
+
 class UserDetailView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
 
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
 
 class RegisterUserView(APIView):
     permission_classes = (permissions.AllowAny,)
@@ -22,56 +24,11 @@ class RegisterUserView(APIView):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse({
-                "success": True,
-                "message": "User registered successfully",
-            })
-        
-        return JsonResponse({
-            "success": False,
-            "message": serializer.errors
-        })
-    
-        # current_user = request.user
-        # if not current_user.is_admin:
-        #     return JsonResponse(
-        #         {
-        #             "success": False,
-        #             "message": "You are not authorized to perform this action",
-        #         },
-        #     )
+            return JsonResponse(
+                {
+                    "success": True,
+                    "message": "User registered successfully",
+                }
+            )
 
-        # user = request.user
-
-        # if "dru" not in request.data:
-        #     return JsonResponse(
-        #         {
-        #             "success": False,
-        #             "message": "DRU is required",
-        #         },
-        #     )
-
-        # dru_id = request.data.get("dru")
-        # try:
-        #     dru_to_register = DRU.objects.filter(id=dru_id).first()
-        #     current_user_dru = user.dru
-        # except DRU.DoesNotExist:
-        #     return JsonResponse(
-        #         {
-        #             "success": False,
-        #             "message": "DRU not found",
-        #         },
-        #     )
-
-        # return JsonResponse(
-        #     {
-        #         "success": True,
-        #         "message": "User registered successfully",
-        #         "data": {
-        #             "email": user.email,
-        #             "first_name": user.first_name,
-        #             "middle_name": user.middle_name,
-        #             "last_name": user.last_name,
-        #         },
-        #     }
-        # )
+        return JsonResponse({"success": False, "message": serializer.errors})
