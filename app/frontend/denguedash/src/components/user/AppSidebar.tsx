5f2df39096e945e26b630e42b50ca3c0f372b7f7
Glen Andrew <gcbulaong@up.edu.ph>
2025-03-10 19:06:17 +0800

    feat: account tab

--- app/frontend/denguedash/src/components/user/AppSidebar.tsx
+++ app/frontend/denguedash/src/components/user/AppSidebar.tsx
@@ -14,6 +14,7 @@ import {
   Sparkles,
   Table,
   TrendingUpDown,
+  UserPlus,
 } from "lucide-react";
 
 import {
@@ -85,6 +86,21 @@ export default function AppSidebar({
     ],
     navMain: isAdmin
       ? [
+          {
+            title: "Registration",
+            url: "registration",
+            icon: UserPlus,
+            items: [
+              {
+                title: "Manage Accounts",
+                url: "/admin/accounts/manage",
+              },
+              {
+                title: "Pending Accounts",
+                url: "/admin/accounts/pending",
+              },
+            ],
+          },
           {
             title: "Analytics",
             url: "analytics",
