a91796fda04dcd6405c852757a685700c3e9b752
Glen Andrew <gcbulaong@up.edu.ph>
2025-03-11 12:09:22 +0800

    feat: accounts table

---
+++ app/frontend/denguedash/src/components/accounts/AccountsTable.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@shadcn/components/ui/table";
+import {
+  UserBriefDetail,
+  UserListPagination,
+} from "@/interfaces/account/user-interface";
+import fetchService from "@/services/fetch.service";
+import { useEffect, useState } from "react";
+import CustomPagination from "../common/CustomPagination";
+
+export default function AccountsTable() {
+  const [users, setUsers] = useState<UserBriefDetail[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const itemsPerPage = 8;
+
+  useEffect(() => {
+    fetchUsers(currentPage);
+  }, [currentPage]);
+
+  const fetchUsers = async (page: number) => {
+    setIsLoading(true);
+    try {
+      const response: UserListPagination = await fetchService.getUsersList(
+        page,
+        itemsPerPage
+      );
+      const data: UserBriefDetail[] = response.results;
+      setUsers(data);
+
+      const totalCount = parseInt(response.count.toString() || "0", 10);
+      setTotalPages(Math.ceil(totalCount / itemsPerPage));
+      console.log(totalPages);
+    } catch (error) {
+      console.error("Failed to fetch users:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+  return (
+    <div className="container mx-auto p-4 border border-gray-200 rounded-lg">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Role</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {isLoading ? (
+            <TableRow>
+              <TableCell colSpan={4} className="text-center">
+                Loading...
+              </TableCell>
+            </TableRow>
+          ) : (
+            users.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.full_name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.role}</TableCell>
+                {/* Todo: Change this shit */}
+                <TableCell>
+                  <button className="text-blue-500">Edit</button>
+                  <button className="text-red-500">Delete</button>
+                </TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+
+      <CustomPagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        handlePageChange={handlePageChange}
+      />
+    </div>
+  );
+}
