7e6f3b0d4a5a0019d5a0a69aeda6a0e0006c8f13
Glen Andrew <gcbulaong@up.edu.ph>
2025-03-11 17:49:56 +0800

    fix: default legacy user

--- app/backend/user/models.py
+++ app/backend/user/models.py
@@ -88,7 +88,7 @@ class User(AbstractBaseUser, PermissionsMixin, BaseModel):
         related_name="user",
     )
     is_admin = models.BooleanField(default=False)
-    is_legacy = models.BooleanField(default=True)
+    is_legacy = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
