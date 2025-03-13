55c458bb5544f79803ae91c6578af9818f23d85c
Glen Andrew <gcbulaong@up.edu.ph>
2025-03-13 23:51:37 +0800

    feat: [frontend] dru operations
    - list of dru
    - delete dru
    - dru profile

--- app/frontend/denguedash/src/interfaces/services/services.interface.ts
+++ app/frontend/denguedash/src/interfaces/services/services.interface.ts
@@ -1,4 +1,11 @@
-export interface BaseServiceResponse {
+interface SuccessResponse {
   success: string;
+}
+
+export interface BaseServiceResponse extends SuccessResponse {
   message: string;
+}
+
+export interface BaseErrorResponse extends SuccessResponse {
+  message: string | { [key: string]: string[] };
 }
