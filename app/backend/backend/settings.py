6bd2f969e64670cd84241ab54b546a45dffd54d6
Glen Andrew <gcbulaong@up.edu.ph>
2025-03-24 22:53:35 +0800

    refactor: change folder name

--- app/backend/backend/settings.py
+++ app/backend/backend/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     "dru",
     "weather",
     "user",
-    "lstm_prediction",
+    "forecasting",
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
