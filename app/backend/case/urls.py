a566ca791c612993788c01a5f330c7a44c9233ed
Glen Andrew <90735671+dreeew05@users.noreply.github.com>
2025-05-13 16:25:17 +0800

    feat: multiple updates
    style: to have a more organized view
    feat: bulk upload path

--- app/backend/case/urls.py
+++ app/backend/case/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views.patient_case_view import PatientCaseView
+from .views.patient_case_view import (
+    PatientCaseView,
+    PatientCaseBulkUploadView,
+)
 from .views.case_report_view import (
     CaseReportView,
     CaseDetailedView,
@@ -15,13 +18,42 @@ from .views.case_count_view import (
 )
 
 urlpatterns = [
-    path("create/", PatientCaseView.as_view(), name="case-create"),
-    path("reports/", CaseReportView.as_view(), name="case-reports"),
-    path("reports/<int:case_id>/", CaseDetailedView.as_view(), name="case-detail"),
-    path("delete/<int:case_id>/", CaseDeleteView.as_view(), name="case-delete"),
-    path("update/<int:case_id>/", CaseUpdateView.as_view(), name="case-update"),
+    path(
+        "create/",
+        PatientCaseView.as_view(),
+        name="case-create",
+    ),
+    path(
+        "create/bulk/",
+        PatientCaseBulkUploadView.as_view(),
+        name="case-create-bulk",
+    ),
+    path(
+        "reports/",
+        CaseReportView.as_view(),
+        name="case-reports",
+    ),
+    path(
+        "reports/<int:case_id>/",
+        CaseDetailedView.as_view(),
+        name="case-detail",
+    ),
+    path(
+        "delete/<int:case_id>/",
+        CaseDeleteView.as_view(),
+        name="case-delete",
+    ),
+    path(
+        "update/<int:case_id>/",
+        CaseUpdateView.as_view(),
+        name="case-update",
+    ),
     # Stats
-    path("stat/", QuickStatisticsView.as_view(), name="case-stat-quick"),
+    path(
+        "stat/",
+        QuickStatisticsView.as_view(),
+        name="case-stat-quick",
+    ),
     path(
         "stat/public/location/",
         DenguePublicLocationStatView.as_view(),
