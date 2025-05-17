584c37653ec7f2cfad0869ef4cb839c6cc957e1e
Glen Andrew <gcbulaong@up.edu.ph>
2025-05-17 11:44:17 +0800

    feat: doker setup

--- app/backend/backend/settings.py
+++ app/backend/backend/settings.py
@@ -32,6 +32,13 @@ CSRF_TRUSTED_ORIGINS = os.environ.get(
     "DJANGO_CSRF_TRUSTED_ORIGINS", "http://127.0.0.1"
 ).split(",")
 
+# Define static and media root/urls for collecting files
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+
 
 # Application definition
 
@@ -95,7 +102,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": "/data/db/db.sqlite3",
     }
 }
 
