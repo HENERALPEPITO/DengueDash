8153b092001f3b903ee85a5f8fdc9662b2603a1d
Glen Andrew <gcbulaong@up.edu.ph>
2025-06-02 13:05:49 +0800

    chore: remove duplicates

--- app/backend/backend/settings.py
+++ app/backend/backend/settings.py
@@ -38,9 +38,6 @@ CSRF_TRUSTED_ORIGINS = os.environ.get(
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
-
 
 # Application definition
 
